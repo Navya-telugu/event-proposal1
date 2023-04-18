a7d5f9b951793eace6e12f67abb70871ea7b44db
Navya-telugu <124676389+Navya-telugu@users.noreply.github.com>
2023-04-18 14:19:44 +0530

    Update connec.js

--- backend/connection/connec.js
+++ backend/connection/connec.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 mongoose.set('strictQuery',false)
 
 async function getConnection(){
-    await mongoose.connect('mongodb+srv://navyatelugu:Cherry@05@cluster0.9lvg6fi.mongodb.net/?retryWrites=true&w=majority');
+//     await mongoose.connect('mongodb+srv://navyatelugu:Cherry@05@cluster0.9lvg6fi.mongodb.net/?retryWrites=true&w=majority');
 }
 
 module.exports=getConnection;
